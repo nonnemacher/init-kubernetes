c2211e12f5ad034a05b3826d9fc07a4498e34a27
nonnemacher <losfueras@gmail.com>
2017-10-24 09:11:54 -0200

    Ajustes no scripts

--- init.sh
+++ init.sh
@@ -1,59 +1,54 @@
 #!/bin/bash
 
+# Configurações
+declare lxd="sudo /snap/bin/lxd"
+declare lxc="sudo /snap/bin/lxc"
+declare get="sudo apt"
+
+# Configurações Kuberntes
 declare network="kubbr0"
+declare storage="kubdir0"
 declare name="kubernetes"
 declare version="ubuntu:16.04"
-
-alias lxd="/snap/bin/lxc"
-alias lxc="/snap/bin/lxc"
-alias get="sudo apt-get"
 
 echo ":::: "
 echo ":::: Remover LXD "
 echo ":::: "
-
-get remove lxd -y
-
-echo ":::: "
-echo ":::: Update Linux "
-echo ":::: "
-
-get update -y
+${get} remove lxd -y
 
 echo ":::: "
 echo ":::: Update Linux "
 echo ":::: "
+${get} update -y
 
-get upgrade -y
+echo ":::: "
+echo ":::: Update Linux "
+echo ":::: "
+${get} upgrade -y
 
 echo ":::: "
 echo ":::: Install Snap "
 echo ":::: "
-
-get install snap -y
+${get} install snap -y
 
 echo ":::: "
 echo ":::: Install Snap - LXD "
 echo ":::: "
-
 sudo snap install lxd
 
 echo ":::: "
 echo ":::: Init Snap - LXD "
 echo ":::: "
-
-sudo lxd init --auto
+${lxd} init --auto
 
 echo ":::: "
 echo ":::: Init Network - LXC - ${network} "
 echo ":::: "
-
-sudo lxc network create ${network} ipv4.address=auto ipv4.nat=true ipv6.address=none ipv6.nat=false
+${lxc} network create ${network} ipv4.address=auto ipv4.nat=true ipv6.address=none ipv6.nat=false
 
 echo ":::: "
 echo ":::: Config Sys "
 echo ":::: "
-
 sudo sysctl fs.inotify.max_user_instances=1048576
 sudo sysctl fs.inotify.max_queued_events=1048576
 sudo sysctl fs.inotify.max_user_watches=1048576
@@ -62,18 +57,35 @@ sudo sysctl vm.max_map_count=262144
 echo ":::: "
 echo ":::: Init LXC - ${name} on ${version} "
 echo ":::: "
-
-sudo lxc init ${version} ${name} -c security.privileged=true -c security.nesting=true -c linux.kernel_modules=ip_tables,ip6_tables,netlink_diag,nf_nat,overlay
+${lxc} init ${version} ${name} -c security.privileged=true -c security.nesting=true -c linux.kernel_modules=ip_tables,ip6_tables,netlink_diag,nf_nat,overlay
 printf "lxc.cap.drop=\nlxc.aa_profile=unconfined\n" | lxc config set kubernetes raw.lxc -
 
 echo ":::: "
 echo ":::: Init Network - ${network} to ${name} "
 echo ":::: "
-
-sudo lxc network attach ${network} ${name}
+${lxc} network attach ${network} ${name}
 
 echo ":::: "
-echo ":::: Init LXC Kubernetes on Ubuntu 16.04 "
+echo ":::: Init LXC ${name} on ${version} "
 echo ":::: "
+${lxc} start kubernetes
+sleep 15
 
-lxc start kubernetes
+echo ":::: "
+echo ":::: Update LXC ${name}-${version}"
+echo ":::: "
+${lxc} exec ${name} -- ${get} remove lxd -y
+${lxc} exec ${name} -- ${get} update
+${lxc} exec ${name} -- ${get} upgrade -y
+${lxc} exec ${name} -- ${get} install squashfuse -y
+${lxc} exec ${name} -- ln -s /bin/true /usr/local/bin/udevadm
+
+echo ":::: "
+echo ":::: LXC ${name}-${version} init lxd "
+echo ":::: "
+${lxc} exec ${name} -- sudo snap install lxd
+${lxc} exec ${name} -- sudo snap install conjure-up --classic
+${lxc} exec ${name} -- sudo /snap/bin/lxd init --auto
+${lxc} exec ${name} -- sudo /snap/bin/lxc network create ${network} ipv4.address=auto ipv4.nat=true ipv6.address=none ipv6.nat=false
+${lxc} exec ${name} -- sudo /snap/bin/lxc storage create ${storage} dir
+${lxc} exec ${name} -- sudo -u ubuntu -i conjure-up kubernetes
