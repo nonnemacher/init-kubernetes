5e9636c6d07c0877a19df5bc5c4451e940c62d71
nonnemacher <losfueras@gmail.com>
2017-11-01 19:28:19 -0200

    ajustes

---
+++ init2.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Ref. -> https://stgraber.org/2017/01/13/kubernetes-inside-lxd/
+
+# Configurações
+declare lxd="sudo /snap/bin/lxd"
+declare lxc="sudo /snap/bin/lxc"
+declare get="sudo apt"
+
+# Configurações Kuberntes
+declare network="kubbr0"
+declare storage="kubdir0"
+declare name="kubernetes"
+declare version="ubuntu:16.04"
+
+echo ":::: "
+echo ":::: Init Network - LXC - ${network} "
+echo ":::: "
+${lxc} network create ${network} ipv4.address=auto ipv4.nat=true ipv6.address=none ipv6.nat=false
+
+echo ":::: "
+echo ":::: Config Sys "
+echo ":::: "
+sudo sysctl fs.inotify.max_user_instances=1048576
+sudo sysctl fs.inotify.max_queued_events=1048576
+sudo sysctl fs.inotify.max_user_watches=1048576
+sudo sysctl vm.max_map_count=262144
+
+echo ":::: "
+echo ":::: Init LXC - ${name} on ${version} "
+echo ":::: "
+${lxc} init ${version} ${name} -c security.privileged=true -c security.nesting=true -c linux.kernel_modules=ip_tables,ip6_tables,netlink_diag,nf_nat,overlay
+printf "lxc.cap.drop=\nlxc.aa_profile=unconfined\n" | lxc config set kubernetes raw.lxc -
+
+echo ":::: "
+echo ":::: Init Network - ${network} to ${name} "
+echo ":::: "
+${lxc} network attach ${network} ${name}
+
+echo ":::: "
+echo ":::: Init LXC ${name} on ${version} "
+echo ":::: "
+${lxc} start kubernetes
+sleep 15
+
+echo ":::: "
+echo ":::: Update LXC ${name}-${version}"
+echo ":::: "
+${lxc} exec ${name} -- ${get} remove lxd -y
+${lxc} exec ${name} -- ${get} update
+${lxc} exec ${name} -- ${get} upgrade -y
+${lxc} exec ${name} -- ${get} install squashfuse -y
+${lxc} exec ${name} -- ln -s /bin/true /usr/local/bin/udevadm
+
+echo ":::: "
+echo ":::: LXC ${name}-${version} init lxd "
+echo ":::: "
+${lxc} exec ${name} -- sudo snap install lxd
+#${lxc} exec ${name} -- sudo snap install conjure-up --classic
+#${lxc} exec ${name} -- sudo /snap/bin/lxd init --auto
+#${lxc} exec ${name} -- sudo /snap/bin/lxc network create ${network} ipv4.address=auto ipv4.nat=true ipv6.address=none ipv6.nat=false
+#${lxc} exec ${name} -- sudo /snap/bin/lxc storage create ${storage} dir
+#${lxc} exec ${name} -- sudo -u ubuntu -i conjure-up kubernetes
---
+++ install-on-lxc.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Configurações
+declare lxd="sudo /snap/bin/lxd"
+declare lxc="sudo /snap/bin/lxc"
+declare get="sudo apt"
+
+# Configurações Node
+declare name="kube"
+declare network="${name}"
+declare version="ubuntu:16.04"
+
+echo ":::: "
+echo ":::: Config Sys "
+echo ":::: "
+sudo sysctl fs.inotify.max_user_instances=1048576
+sudo sysctl fs.inotify.max_queued_events=1048576
+sudo sysctl fs.inotify.max_user_watches=1048576
+sudo sysctl vm.max_map_count=262144
+
+echo ":::: "
+echo ":::: Init LXC - ${name} on ${version} "
+echo ":::: "
+${lxc} init ${version} ${name} -c security.privileged=true -c security.nesting=true -c linux.kernel_modules=ip_tables,ip6_tables,netlink_diag,nf_nat,overlay
+#printf "lxc.cap.drop=\nlxc.aa_profile=unconfined\n" | ${lxc} config set kubernetes raw.lxc -
+
+sleep 5
+
+echo ":::: "
+echo ":::: Init Network - LXC - ${network} "
+echo ":::: "
+${lxc} network create ${network} ipv4.address=auto ipv4.nat=true ipv6.address=none ipv6.nat=false
+
+sleep 5
+
+echo ":::: "
+echo ":::: Init Network - ${network} to ${name} "
+echo ":::: "
+${lxc} network attach ${network} ${name}
+
+echo ":::: "
+echo ":::: Init LXC ${name} on ${version} "
+echo ":::: "
+${lxc} start ${name}
+sleep 10
+
+echo ":::: "
+echo ":::: Update LXC ${name}-${version}"
+echo ":::: "
+${lxc} exec ${name} -- ${get} remove lxd -y
+${lxc} exec ${name} -- ${get} update
+${lxc} exec ${name} -- ${get} upgrade -y
+${lxc} exec ${name} -- ${get} install squashfuse curl apt-transport-https docker.io -y
+${lxc} exec ${name} -- ln -s /bin/true /usr/local/bin/udevadm
+
+echo ":::: "
+echo ":::: LXC ${name}-${version} init lxd "
+echo ":::: "
+${lxc} exec ${name} -- sudo snap install lxd
+${lxc} exec ${name} -- sh -c "curl https://packages.cloud.google.com/apt/doc/apt-key.gpg > /root/apt-key.gpg"
+${lxc} exec ${name} -- apt-key add /root/apt-key.gpg
+${lxc} exec ${name} -- sh -c "echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' > /etc/apt/sources.list.d/kubernetes.list"
+${lxc} exec ${name} -- ${get} update 
+${lxc} exec ${name} -- ${get} install kubelet kubeadm kubectl kubernetes-cni -y
+
+
